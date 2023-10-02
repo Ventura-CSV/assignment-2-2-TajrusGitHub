078c7a92e3c9a0bcf0ff630bf7a4c56e351e2ff8
Tajrus Lipscomb Jr. <tajrus_lipscomb1@my.vcccd.edu>
2023-10-02 11:12:44 -0700

    Assignment 2-2

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
 def main():
-    """
-    ##################################################
-    # Comlete your code here
-    Use the same variables: celcius fahrenheit 
-    ##################################################
-    """
+    # Ask the user to enter a temperature in Celsius
+    celsius = float(input("Enter temperature in Celsius: 42"))
 
-    """
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    """
-    return celcius, fahrenheit
+    # Convert Celsius to Fahrenheit
+    fahrenheit = celsius_to_fahrenheit(celsius)
 
+    # Display the temperature in Fahrenheit with two decimal places
+    print(f"Fahrenheit: {fahrenheit:.2f}")
+
+    # Return the values (optional, as not specified in the instructions)
+    return celsius, fahrenheit
 
 if __name__ == '__main__':
     main()
